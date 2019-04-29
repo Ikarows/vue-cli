605373bba19506cb9f69f2fe372847a7f9b72bc8
Ikarows <1254803307@qq.com>
2019-04-29 23:18:23 +0800

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -3,9 +3,9 @@ import prod from '@/config/prod.env'
 let config = null;
 
 if (process.env.NODE_ENV == 'development') {
-  config = prod//开发环境请求地址
+  config = dev; //开发环境请求地址
 } else if (process.env.NODE_ENV == 'production') {
-  config = dev//生产环境请求地址
+  config = prod; //生产环境请求地址
 }
 
 export default config;
